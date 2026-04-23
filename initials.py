566fb4d11d00481b0fabe231964adcdb239f0f47
karlmal20 <karleymalveo@yahoo.com>
2026-04-22 23:39:48 -0500

    Create initials.py with ASCII art
    Add initials art for Karley Malveo

---
+++ initials.py
@@ -0,0 +1,19 @@
+#Karley Malveo
+#I like to bake
+first='''k   k
+k  k 
+k k  
+kk   
+k k  
+k  k 
+k   k'''
+last='''m   m
+mm mm
+mm mm
+m m m
+m   m
+m   m
+m   m'''
+print(first)
+print('')
+print(last)
