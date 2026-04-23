f45829e60135286229b70e808743a10a30fd4323
karlmal20 <karleymalveo@yahoo.com>
2026-04-22 23:40:38 -0500

    Add initials.cpp to display initials in ASCII art

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// Fun fact: I like to bake
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "k   k  m   m\n";
+  cout << "k  k   mm mm\n";
+  cout << "k k    mm mm\n";
+  cout << "kk     m m m\n";
+  cout << "k k    m   m\n";
+  cout << "k  k   m   m\n";
+  cout << "k   k  m   m\n";
+}
